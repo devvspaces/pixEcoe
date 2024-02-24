19db07b754ba079aa967fc4f9c2526ab7e8e378c
Jeffery Kizarmie Adolor <jefferyadolor18@gmail.com>
2024-02-24 18:59:06 +0100

    regerated native folders

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/lilkizarmie/GexcoEcoe/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/pixEcoe/app/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
